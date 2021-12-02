dc6a58f98744fae8de505831f585c2c0dc6183d5
Eric Santos <eric.silvasantos44@gmail.com>
2021-12-02 15:16:06 -0300

    Finalizada a segunda funçao

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,15 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(primeiro,segundo) {
+  if(primeiro===true && segundo===true){
+      return true;
+  }else{
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base,height) {
+  return((base*height)/2)
 }
 
 // Desafio 3
