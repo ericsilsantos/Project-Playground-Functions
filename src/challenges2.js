423072e148b84d31c00d0b1998023f9a39c158e7
Eric Santos <eric.silvasantos44@gmail.com>
2021-12-08 12:18:44 -0300

    Adicionado o decimo primeiro desafio

--- src/challenges2.js
+++ src/challenges2.js
@@ -31,8 +31,38 @@ function techList(lista, nome) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(lista) {
+  if(lista.length!==11){
+    return("Array com tamanho incorreto.")
+  }
+  let contador=1
+  for (let index=0;index<lista.length;index+=1){
+    if (lista[index]<0 || lista[index]>9 || contador>=3){
+      return("não é possível gerar um número de telefone com esses valores")
+    }
+    contador=1
+    for(let index2=index+1;index2<lista.length;index2+=1){
+      if(lista[index]===lista[index2]){
+        contador+=1
+      }
+    }
+  }
+  return(gerarNumero(lista))
+}
+//criei uma função para criar uma string com a formatação de um numero de telefone
+function gerarNumero(lista){
+    let numero="("
+    for(let index=0; index<lista.length; index+=1){
+      switch(index){
+        case 2:
+          numero+=") "
+          break
+        case 7:
+          numero+="-"
+      }
+      numero+=lista[index]
+    }
+    return numero;
 }
 
 // Desafio 12
